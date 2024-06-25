78e76e6ab30c4a61dd7e235858fbf9aa3b36f604
Jairo Herrera <jairo.herrera729@pascualbravo.edu.co>
2024-06-25 02:14:55 -0500

    The framework change was made and the code was refactored, section 2 has progress!

--- src/app/pages/portfolio-page/portfolio-page.component.ts
+++ src/app/pages/portfolio-page/portfolio-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { CardComponent } from '../../components/card/card.component';
+import { SecondaryHeaderComponent } from '../../components/secondary-header/secondary-header.component';
 
 @Component({
   selector: 'app-portfolio-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CardComponent, SecondaryHeaderComponent],
   templateUrl: './portfolio-page.component.html',
   styleUrl: './portfolio-page.component.css',
 })
